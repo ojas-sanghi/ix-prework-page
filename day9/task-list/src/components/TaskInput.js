f7e58be5368c3abde8d6e717116dc01b5cef2f61
Ojas Sanghi <ojas.sanghi@protonmail.com>
2022-07-21 11:00:46 -0700

    Add base day 9 wrok

---
+++ day9/task-list/src/components/TaskInput.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+
+import { Task } from "../models/task";
+
+export default function TaskInput(props) {
+  const [taskName, setTaskName] = useState("");
+
+  function onFormSubmitted(event) {
+    event.preventDefault();
+
+    // create a new task
+    const task = new Task(null, taskName, false);
+
+    props.onTaskCreated(task);
+    setTaskName("");
+  }
+
+  return (
+    <div className="mt-4">
+      <form onSubmit={onFormSubmitted}>
+        <div className="input-group mb-3">
+          <input
+            value={taskName}
+            onChange={(event) => {
+              setTaskName(event.target.value);
+            }}
+            type="text"
+            className="form-control"
+            placeholder="Enter task name"
+          />
+
+          <button className="btn btn-outline-secondary" type="submit">
+            +
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
---
+++ day9/task-list/src/firebase/firebase.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBSz1TUhgINLiiDXhjmnA3w3TUDAz6-vXk",
+  authDomain: "task-list-ix.firebaseapp.com",
+  projectId: "task-list-ix",
+  storageBucket: "task-list-ix.appspot.com",
+  messagingSenderId: "297940695005",
+  appId: "1:297940695005:web:4f9ff1e218ac02b5886834",
+  measurementId: "G-Z6YM751EB5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+const firestore = getFirestore(app);
+export { firestore };
